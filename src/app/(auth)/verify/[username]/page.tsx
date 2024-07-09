0134398c1764d06b00e7989f490efedcac03811a
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-07-09 11:20:16 +0530

    verify/[username] added

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { ApiResponse } from '@/types/ApiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { verifySchema } from '@/schemas/verifySchema';
+
+export default function VerifyAccount() {
+  const router = useRouter();
+//   get the username from the URL
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+//   create a form with the verification schema
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+//   method to submit the verification code
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+        // send a POST request to the API to verify the code
+      const response = await axios.post<ApiResponse>(`/api/verify-code`, {
+        // pass the username and code to the API
+        username: params.username,
+        code: data.code,
+      });
+    //   show a success message and redirect to the sign-in page
+      toast({
+        title: 'Success',
+        description: response.data.message,
+      });
+
+      router.replace('/sign-in');
+    } catch (error) {
+        // show an error message if the verification fails
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: 'Verification Failed',
+        description:
+          axiosError.response?.data.message ??
+          'An error occurred. Please try again.',
+        variant: 'destructive',
+      });
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
